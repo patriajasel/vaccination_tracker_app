4477540c66568e67ba07976b186d96e5520952b5
Jasel <patriajasel@gmail.com>
2025-01-26 11:25:34 +0800

    Navigation update

--- lib/pages/app_navigation.dart
+++ lib/pages/app_navigation.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vaccination_tracker_app/pages/home_page.dart';
 import 'package:vaccination_tracker_app/pages/my_child_page.dart';
 import 'package:vaccination_tracker_app/pages/profile_page.dart';
 import 'package:vaccination_tracker_app/pages/rhu_page.dart';
 
-class AppNavigation extends StatefulWidget {
+/**
+ * TODO 
+ *  
+ * ! Double click to exit the app
+ */
+
+class AppNavigation extends ConsumerStatefulWidget {
   const AppNavigation({super.key});
 
   @override
-  State<AppNavigation> createState() => _AppNavigationState();
+  ConsumerState<AppNavigation> createState() => _AppNavigationState();
 }
 
-class _AppNavigationState extends State<AppNavigation> {
+class _AppNavigationState extends ConsumerState<AppNavigation> {
   final List<Widget> screens = [
     const HomePage(),
     const RhuPage(),
@@ -21,37 +28,44 @@ class _AppNavigationState extends State<AppNavigation> {
 
   int selectedNavIndex = 0;
 
+  DateTime? lastPressedTime;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Theme(
-          data: ThemeData(
-              navigationBarTheme: NavigationBarThemeData(
-                  backgroundColor: Colors.blue.shade700,
-                  indicatorColor: Colors.blue,
-                  labelTextStyle: WidgetStateProperty.all(const TextStyle(
-                      color: Colors.white, fontFamily: 'Mali', fontSize: 12.0)),
-                  iconTheme: WidgetStateProperty.all(
-                      const IconThemeData(color: Colors.white)))),
-          child: NavigationBar(
-              selectedIndex: selectedNavIndex,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedNavIndex = index;
-                });
-              },
-              destinations: const [
-                NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-                NavigationDestination(
-                    icon: Icon(Icons.local_hospital), label: "RHU Schedule"),
-                NavigationDestination(
-                    icon: Icon(Icons.child_care), label: "My Child"),
-                NavigationDestination(
-                    icon: Icon(Icons.person), label: "Profile"),
-              ])),
-      body: IndexedStack(
-        index: selectedNavIndex,
-        children: screens,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        bottomNavigationBar: Theme(
+            data: ThemeData(
+                navigationBarTheme: NavigationBarThemeData(
+                    backgroundColor: Colors.cyan.shade300,
+                    indicatorColor: Colors.cyan,
+                    labelTextStyle: WidgetStateProperty.all(const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Mali',
+                        fontSize: 12.0)),
+                    iconTheme: WidgetStateProperty.all(
+                        const IconThemeData(color: Colors.white)))),
+            child: NavigationBar(
+                selectedIndex: selectedNavIndex,
+                onDestinationSelected: (index) {
+                  setState(() {
+                    selectedNavIndex = index;
+                  });
+                },
+                destinations: const [
+                  NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+                  NavigationDestination(
+                      icon: Icon(Icons.local_hospital), label: "RHU Schedule"),
+                  NavigationDestination(
+                      icon: Icon(Icons.child_care), label: "My Child"),
+                  NavigationDestination(
+                      icon: Icon(Icons.person), label: "Profile"),
+                ])),
+        body: IndexedStack(
+          index: selectedNavIndex,
+          children: screens,
+        ),
       ),
     );
   }
