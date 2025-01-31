7e9c4f29cbcee9198f78c17ec0e6668dd0928e9b
Jasel <patriajasel@gmail.com>
2025-01-31 16:54:42 +0800

    Calendar Page Update

--- lib/pages/all_calendar_page.dart
+++ lib/pages/all_calendar_page.dart
@@ -1,106 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:vaccination_tracker_app/models/child_schedules.dart';
+import 'package:vaccination_tracker_app/services/riverpod_services.dart';
+import 'package:vaccination_tracker_app/utils/show_marked_dates.dart';
 import 'package:vaccination_tracker_app/utils/text_style.dart';
 
-class AllCalendarPage extends StatelessWidget {
+class AllCalendarPage extends ConsumerWidget {
   const AllCalendarPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
+    final themeColor = ref.watch(themeProvider);
 
     DateTime dateToday = DateTime.now();
 
-    // List of months to display
-    final List<int> months = List.generate(12, (index) => index + 1);
+    // Precompute the first and last days of each month to avoid recalculating in the builder.
+    final List<Map<String, DateTime>> months = List.generate(
+      12,
+      (index) {
+        final month = index + 1;
+        return {
+          'firstDay': DateTime(dateToday.year, month, 1),
+          'lastDay': DateTime(dateToday.year, month + 1, 0),
+        };
+      },
+    );
+
+    final currentDate =
+        DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
+    final schedules = ref.watch(rpChildScheds.notifier).childScheds;
+
+    List<DateTime> scheduleDate =
+        schedules.map((schedule) => schedule.schedDate).toList();
+
+    List<ScheduleModel> scheduleToday = [];
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text("All Schedules"),
         titleTextStyle: const TextStyle(
-            color: Colors.black,
-            fontFamily: "RadioCanada",
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold),
+          color: Colors.black,
+          fontFamily: "RadioCanada",
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
         centerTitle: true,
-        backgroundColor: Colors.cyan.shade300,
+        backgroundColor: themeColor,
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
       ),
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Colors.cyan.shade300,
+              themeColor,
               Colors.white,
             ],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
-        child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.only(top: screenHeight * 0.01),
-            child: ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: months.length,
-              itemBuilder: (context, index) {
-                final month = months[index];
-                final firstDayOfMonth = DateTime(dateToday.year, month, 1);
-                final lastDayOfMonth = DateTime(dateToday.year, month + 1, 0);
+        child: ListView.builder(
+          padding: EdgeInsets.only(top: screenHeight * 0.01),
+          itemCount: months.length,
+          itemBuilder: (context, index) {
+            final firstDay = months[index]['firstDay']!;
+            final lastDay = months[index]['lastDay']!;
 
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                          vertical: screenHeight * 0.01,
-                          horizontal: screenWidth * 0.025),
-                      child: TableCalendar(
-                        calendarFormat: CalendarFormat.month,
-                        daysOfWeekHeight: screenHeight * 0.04,
-                        headerStyle: const HeaderStyle(
-                          leftChevronVisible: false,
-                          rightChevronVisible: false,
-                          formatButtonVisible: false,
-                          titleCentered: true,
-                          titleTextStyle: TextStyle(
-                            fontFamily: "RadioCanada",
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
+            return Container(
+              margin: EdgeInsets.symmetric(
+                  vertical: screenHeight * 0.01,
+                  horizontal: screenWidth * 0.025),
+              child: TableCalendar(
+                calendarFormat: CalendarFormat.month,
+                daysOfWeekHeight: screenHeight * 0.04,
+                headerStyle: const HeaderStyle(
+                  leftChevronVisible: false,
+                  rightChevronVisible: false,
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                  titleTextStyle: TextStyle(
+                    fontFamily: "RadioCanada",
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                  weekdayStyle: TextStyles().daysOfWeek,
+                  weekendStyle: TextStyles().daysOfWeek,
+                ),
+                calendarStyle: CalendarStyle(
+                  defaultTextStyle: TextStyles().calendarDays,
+                  todayTextStyle: TextStyles().calendarDays,
+                  todayDecoration: BoxDecoration(
+                    color: Colors.cyan.shade50,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  weekendTextStyle: TextStyles().calendarDays,
+                ),
+                onDaySelected: (selectedDay, focusedDay) {
+                  scheduleToday
+                    ..clear
+                    ..addAll(
+                      schedules.where((schedule) =>
+                          schedule.schedDate.year == selectedDay.year &&
+                          schedule.schedDate.month == selectedDay.month &&
+                          schedule.schedDate.day == selectedDay.day),
+                    );
+
+                  showDialog(
+                      context: context,
+                      builder: (context) => MarkedDayDialog(
+                          screenHeight: screenHeight,
+                          screenWidth: screenWidth,
+                          currentDate: currentDate,
+                          schedToday: scheduleToday));
+                },
+                calendarBuilders: CalendarBuilders(
+                  markerBuilder: (context, day, events) {
+                    if (scheduleDate
+                        .any((selectedDay) => isSameDay(selectedDay, day))) {
+                      return Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Container(
+                          width: 8.0,
+                          height: 8.0,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
                           ),
                         ),
-                        daysOfWeekStyle: DaysOfWeekStyle(
-                          weekdayStyle: TextStyles().daysOfWeek,
-                          weekendStyle: TextStyles().daysOfWeek,
-                        ),
-                        calendarStyle: CalendarStyle(
-                          defaultTextStyle: TextStyles().calendarDays,
-                          todayTextStyle: TextStyles().calendarDays,
-                          todayDecoration: BoxDecoration(
-                            color: Colors.cyan.shade50,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          weekendTextStyle: TextStyles().calendarDays,
-                        ),
-                        focusedDay:
-                            firstDayOfMonth, // Set focus for the first day of the month
-                        firstDay: firstDayOfMonth, // First day of the month
-                        lastDay: lastDayOfMonth, // Last day of the month
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
+                      );
+                    }
+                    return null;
+                  },
+                ),
+                focusedDay: firstDay,
+                firstDay: firstDay,
+                lastDay: lastDay,
+              ),
+            );
+          },
         ),
       ),
     );
