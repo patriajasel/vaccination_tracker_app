0a670a1ac8f1e9a11673d0f11a082b85a2b7d724
Jasel <patriajasel@gmail.com>
2025-01-31 16:52:07 +0800

    Services Update

--- lib/services/riverpod_services.dart
+++ lib/services/riverpod_services.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vaccination_tracker_app/models/child_information.dart';
+import 'package:vaccination_tracker_app/models/child_schedules.dart';
 import 'package:vaccination_tracker_app/models/guardian_information.dart';
 import 'package:vaccination_tracker_app/models/user_information.dart';
+import 'package:vaccination_tracker_app/models/vaccine_data.dart';
 
 /** 
  * TODO SECTION
@@ -23,3 +26,49 @@ final rpUserInfo = ChangeNotifierProvider<UserInformation>((ref) {
 });
 
 final childImageLink = StateProvider<List<String>>((ref) => []);
+
+final rpChildScheds = ChangeNotifierProvider<ChildSchedules>(
+  (ref) {
+    return ChildSchedules([]);
+  },
+);
+
+final rpVaccineData = StateProvider<List<VaccineData>>((ref) => []);
+
+final currentDateProvider = StateProvider<DateTime>((ref) =>
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day));
+
+final schedulePastToday = StateProvider<List<ScheduleModel>>((ref) => []);
+
+final vaccineTrackerProvider = StateProvider<List<Map<String, dynamic>>>(
+  (ref) => [
+    {'title': 'BCG', 'checked': false, 'color': Colors.white},
+    {'title': 'Penta 1', 'checked': false, 'color': Colors.white},
+    {'title': 'Hepa B', 'checked': false, 'color': Colors.white},
+    {'title': 'Penta 2', 'checked': false, 'color': Colors.white},
+    {'title': 'OPV 1', 'checked': false, 'color': Colors.white},
+    {'title': 'Penta 3', 'checked': false, 'color': Colors.white},
+    {'title': 'OPV 2', 'checked': false, 'color': Colors.white},
+    {'title': 'PCV 1', 'checked': false, 'color': Colors.white},
+    {'title': 'OPV 3', 'checked': false, 'color': Colors.white},
+    {'title': 'PCV 2', 'checked': false, 'color': Colors.white},
+    {'title': 'IPV 1', 'checked': false, 'color': Colors.white},
+    {'title': 'PCV 3', 'checked': false, 'color': Colors.white},
+    {'title': 'IPV 2', 'checked': false, 'color': Colors.white},
+    {'title': 'MMR', 'checked': false, 'color': Colors.white},
+  ],
+);
+
+final isTodayScheduled = StateProvider<bool>((ref) => false);
+final isTomorrowScheduled = StateProvider<bool>((ref) => false);
+final isWithinTwoDays = StateProvider<bool>((ref) => false);
+final isWithinThreeDays = StateProvider<bool>((ref) => false);
+final nextSchedule = StateProvider<DateTime?>((ref) => null);
+
+final themeProvider = StateProvider<Color>(
+  (ref) => Colors.cyan.shade300,
+);
+
+final navIndicatorProvider = StateProvider<Color>(
+  (ref) => Colors.cyan,
+);
